fe964a8aee8d16790bf5f865b98367797d8ccf60
maxgatti535-cyber <maxgatti535@gmail.com>
2025-11-27 18:57:19 +0100

    Update src.App.tsx

--- src/src.App.tsx
+++ src/src.App.tsx
@@ -1,3 +1,22 @@
-export default function App() {
-  return <h1 style={{ color: 'red', fontSize: '40px' }}>TEST FUNZIONA!</h1>
+import React, { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Dashboard from './components/Dashboard'
+
+const App: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState('dashboard')
+
+  return (
+    <div className="bg-creamBg min-h-screen flex flex-col">
+      <Header onNavigate={setCurrentPage} currentPage={currentPage} />
+      
+      <main className="flex-grow p-4">
+        {currentPage === 'dashboard' && <Dashboard />}
+      </main>
+      
+      <Footer onNavigate={setCurrentPage} />
+    </div>
+  )
 }
+
+export default App
